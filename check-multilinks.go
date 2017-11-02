8cc237351d4617017c7164e7a0439497288cfb06
Steve Kemp <steve@steve.org.uk>
2017-11-02 19:01:35 +0200

    Added link-strategy plugin.
    This tests for people spamming links with markdown, bbcode, html, etc.

---
+++ check-multilinks.go
@@ -0,0 +1,68 @@
+//
+// Check that there are not too many different attempts at adding a URL
+//
+
+package main
+
+import (
+	"index/suffixarray"
+	"regexp"
+)
+
+func init() {
+
+	//
+	// Add our plugin-method
+	//
+	x := Plugins{Name: "50-multilinks.js",
+		Description: "Look for different linking strategies.",
+		Author:      "Steve Kemp <steve@steve.org.uk>",
+		Test:        checkHyperlinkCounts}
+	plugins = append(plugins, x)
+
+}
+
+//
+// Look for multiple linking strategies
+//
+func checkLinkingTypes(x Submission) string {
+
+	//
+	// The things we're looking for
+	//
+	patterns := []string{"<a href=\"https?:",
+		"\\[?url=https?:",
+		"\\[?link=https?:",
+		"[ \t]https?:/" }
+
+	//
+	// Count of types we've found thus far
+	//
+	count := 0
+
+	//
+	// For each pattern.
+	//
+	for _,p := range( patterns ) {
+
+		// Look for matches
+		r := regexp.MustCompile(p)
+
+		index := suffixarray.New([]byte(x.Comment))
+		matches := index.FindAllIndex(r, -1)
+
+		if ( len(matches) > 0 ) {
+			count += 1
+		}
+	}
+
+
+	if count >= 3 {
+		return ("Multiple linking strategies")
+	}
+
+	//
+	// All OK
+	//
+	return ""
+}
---
+++ check-multilinks_test.go
@@ -0,0 +1,55 @@
+//
+// Test for our link-method plugin.
+//
+
+package main
+
+import (
+	"testing"
+)
+
+//
+// Test that we can accept random linking-methods.
+//
+func TestLinkTypesOK(t *testing.T) {
+
+	//
+	//  Each type alone is OK
+	//
+	input := []string{"<a href=\"https://example.com\">test</a>",
+		"[url=https://exmapl.eocm]title[/url]",
+		"[link=https://moi.com/]steve[/link]:",
+		"[ \t]https?:/" }
+
+
+	//
+	// Try them all
+	//
+	for _,i := range(input) {
+		//
+		// Test a simple comment.
+		//
+		result := checkLinkingTypes(Submission{Comment: i})
+		if len(result) != 0 {
+			t.Errorf("Unexpected response: '%v'", result)
+		}
+	}
+}
+
+
+
+//
+// Test that all methods are too much.
+//
+func TestLinkTypesSpam(t *testing.T) {
+
+	//
+	//  Try all three
+	//
+	input := "<a href=\"https://steve.fi/\">Steve Kemp</a>, [url=http://moi.kisssa]Finnihs[/url]  http://bare.link.com/"
+
+	result := checkLinkingTypes(Submission{Comment: input})
+	if len(result) == 0 {
+		t.Errorf("Unexpected response: '%v'", result)
+	}
+}
