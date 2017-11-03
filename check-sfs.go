87fe8f5b82f3edbd5ac3fcf73b059504b859632c
Steve Kemp <steve@steve.org.uk>
2017-11-03 20:16:56 +0200

    Added stopforumspam.com test.
    This updates #4.

---
+++ check-sfs.go
@@ -0,0 +1,93 @@
+//
+//  Check for an IP that is in the stopforumspam.com blacklist.
+//
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"strings"
+	"time"
+)
+
+func init() {
+	//
+	// Add our plugin-method.
+	//
+	var x = Plugins{Name: "80-sfs.js",
+		Description: "Look for blacklisted IPs via stopforumspam.com",
+		Author:      "Steve Kemp <steve@steve.org.uk>",
+		Test:        checkSFSBlacklist}
+	plugins = append(plugins, x)
+
+}
+
+//
+// Lookup the IP address of the submitter in the stopforumspam.com blacklist.
+//
+func checkSFSBlacklist(x Submission) (PluginResult, string) {
+
+	//
+	// See if we have an IPv4 address.
+	//
+	regex := regexp.MustCompile("^([0-9]+).([0-9]+).([0-9]+).([0-9]+)$")
+	match := regex.FindStringSubmatch(x.IP)
+
+	//
+	// If that failed then we know we have an IPv6-address, or a missing
+	// address, so we terminate
+	//
+	if len(match) <= 0 {
+		return Undecided, ""
+	}
+
+	//
+	// Build a client with sane timeout
+	//
+	var netClient = &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	//
+	// The URL we'll fetch
+	//
+	url := fmt.Sprintf("http://www.stopforumspam.com/api?ip=%s", x.IP)
+
+	//
+	// Make the request
+	//
+	response, err := netClient.Get( url )
+
+	//
+	// Handle error
+	//
+	if ( err != nil ) {
+		fmt.Printf("HTTP-Error from %s - %s", url, err)
+		return Error, err.Error()
+	}
+
+	//
+	// Ensure we close the body
+	//
+        defer response.Body.Close()
+        contents, err := ioutil.ReadAll(response.Body)
+        if err != nil {
+		fmt.Printf("HTTP-Error from %s - %s", url, err)
+		return Error,err.Error()
+        }
+
+	//
+	// Does it appear?
+	//
+	if ( strings.Contains( string(contents), "<appears>yes</appears>" ) ) {
+		return Spam, "Listed in StopForumSpam.com"
+	}
+
+	//
+	// Not listed
+	//
+	return Undecided, ""
+}
---
+++ check-sfs_test.go
@@ -0,0 +1,47 @@
+//
+// Test for our stopforumspam.com-lookup plugin.
+//
+
+package main
+
+import (
+	"testing"
+)
+
+//
+// Test IPs that should never be listed.
+//
+func TestNonSpammer(t *testing.T) {
+
+	//
+	// Test several IPs
+	//
+	inputs := []string{"127.0.0.1",	"192.168.0.1" }
+
+	for _, input := range inputs {
+
+		result, detail := checkSFSBlacklist(Submission{IP: input})
+
+		if result != Undecided {
+			t.Errorf("Unexpected response: '%v'", result)
+		}
+		if len(detail) != 0 {
+			t.Errorf("Unexpected response: '%v'", detail)
+		}
+	}
+}
+
+//
+// Test a known-bad IP
+//
+func TestSFSListed(t *testing.T) {
+
+	result, detail := checkSFSBlacklist(Submission{IP: "37.115.125.139"})
+
+	if result != Spam {
+		t.Errorf("Unexpected response: '%v'", result)
+	}
+	if len(detail) == 0 {
+		t.Errorf("Unexpected response: '%v'", detail)
+	}
+}
